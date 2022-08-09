adce95733aaa0a4d42c8078b87cd753a8fa7f825
Tawan Alves de Souza <110352514+TawanAlves@users.noreply.github.com>
2022-08-09 17:27:49 -0300

    aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50.5
+console.log(`A velocidade do seu carro é ${vel} Km/h`) 
+if (vel > 60){ //Condição simples
+    console.log("Você utrapassou a velocidade permitida. Multado")
+}
+console.log('Dirija sempr usando cinto de segurança')
