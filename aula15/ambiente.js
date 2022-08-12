a9385780f318a7e3e3e9e278df57693996e54320
Tawan Alves de Souza <110352514+TawanAlves@users.noreply.github.com>
2022-08-12 00:15:16 -0300

    aula15

---
+++ aula15/ambiente.js
@@ -0,0 +1,19 @@
+let num = [5, 8, 2, 9, 3]
+num [4] = 99 //coloca numero na posiç~ao 7
+num.push(88) //adiciona número na últma posição
+
+
+console.log(`__Nosso vetor é o ${num}`)
+//console.log (`__O vetor tem ${num.length} posições`) //diz tamanho do vetor
+//console.log(`__Ordenando: ${num.sort()}`) //coloca na ordem
+
+for(let pos = 0; pos < num.length; pos++){
+    console.log(`a posição ${pos} tem o vaçor ${num[pos]}`)
+}
+console.log(`  `)
+console.log( num.indexOf(9) )//diz a posição q o 9 está
+console.log( num.indexOf(1) )//coloca -1 se n tiver o número
+console.log(`  `)
+for(let pos in num ){
+    console.log(`a posição ${pos} tem o vaçor ${num[pos]}`)
+}
