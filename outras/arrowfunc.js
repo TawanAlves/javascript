9db1825d27d0a586d8da21246cfc48ae6db47dda
Tawan Alves de Souza <110352514+TawanAlves@users.noreply.github.com>
2022-08-20 00:24:41 -0300

    arrow function

---
+++ outras/arrowfunc.js
@@ -0,0 +1,7 @@
+/*function sum(a, b) {
+ return a+b
+}
+console.log (sum(5,5))
+*/
+const sum = (a, b) => a+b; //arrow fiunction
+console.log (sum(5,4));
---
+++ outras/classes.js
@@ -0,0 +1,12 @@
+/*const cliente1Nome = "Ricardo";
+const cliente1CPF = 11122233309;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Alice";
+const client2Cpf = 88822233309;
+const client2Agencia = 1001;
+const client2Saldo = 0;
+
+console.log(cliente1Nome);
+*/
