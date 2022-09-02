88d775652a024ac2fcc7beabd4fc3046a63f825b
Tawan Alves de Souza <110352514+TawanAlves@users.noreply.github.com>
2022-09-02 16:49:51 -0300

    cronometro on

---
+++ cronômetro/script.js
@@ -0,0 +1,38 @@
+let segundos = 0;
+let minutos = 0;
+var intervalo;
+
+function mostrardois(digit){
+   if(digit < 10){
+      return('0'+digit);
+   }else{
+      return (digit)
+   }
+}
+
+function reset(){
+   clearInterval ( intervalo);   
+   segundos = 0;
+   minutos = 0;
+   document.getElementById('minutos').innerText =mostrardois(segundos);
+   document.getElementById('segundos').innerText = mostrardois(minutos);
+      
+}
+
+function start(){
+ intervalo =  setInterval(contar, 1000);
+}
+
+function stop(){
+   clearInterval ( intervalo);
+}
+function contar(){
+   segundos++;
+   document.getElementById('segundos').innerText=mostrardois(segundos);
+   if(segundos == 59){
+      minutos++;
+      document.getElementById('minutos').innerText = mostrardois(minutos);
+      segundos = 0;
+   }
+  
+}
